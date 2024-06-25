b9a462b0ccd6c80077934d181cf05eda8d746edb
Kec3-Lin3 <keceline74@gmail.com>
2024-06-25 11:35:26 -0300

    fix: erro de merge corrigido que fez aparecer o id_occur Co-authored-by: Kece <keceline74@gmail.com> Co-authored-by: Ana Júlia <anaju.batsouza@gmail.com> Co-authored-by: izabellerivas <izabelle.rivas123@gmail.com>

--- backend/src/protocols.ts
+++ backend/src/protocols.ts
@@ -35,13 +35,11 @@ export type OccurrenceData_bd = {
     violence_type: string
 }
 export type ViolenceState = {
-    id_occur: bigint,
     uf_state:string,
     city: string
 };
 // Tipo para receber no json (json não tem tipo data)
 export type AboutViolence = {
-    id_occur: bigint,
     date_violence_s: string,
     agegroup: string,
     time_violence_s: string,
@@ -50,10 +48,4 @@ export type AboutViolence = {
 export type ClassifyViolencePage = {
     violencesoptions: string
 }
-export type LocalViolence = {
-    id_occur: bigint,
-    latitude: number,
-    longitude: number
-};
-
 
--- backend/src/repositories/formAboutViolencePage-repository.ts
+++ backend/src/repositories/formAboutViolencePage-repository.ts
@@ -4,22 +4,23 @@ import { Decimal } from '@prisma/client/runtime/library';
 
 const prisma = new PrismaClient();
 
-async function AboutViolenceOccurrence(aboutviolence: AboutViolence): Promise<occurrence> {
-    const {id_occur, date_violence_s, agegroup,time_violence_s} = aboutviolence;
-    const occurrence = await prisma.occurrence.update({
-        where: {
-            id_occurrence: id_occur
-        },
-        data:{
-            date_violence: date_violence_s,
-            time_violence: time_violence_s,
-            age_group: agegroup,
-        }
-    })
-    return occurrence
-}
+// essas funcoes sao antigas e não usamos mais, por isso esta comentado
+// async function AboutViolenceOccurrence(aboutviolence: AboutViolence): Promise<occurrence> {
+//     const {id_occur, date_violence_s, agegroup,time_violence_s} = aboutviolence;
+//     const occurrence = await prisma.occurrence.update({
+//         where: {
+//             id_occurrence: id_occur
+//         },
+//         data:{
+//             date_violence: date_violence_s,
+//             time_violence: time_violence_s,
+//             age_group: agegroup,
+//         }
+//     })
+//     return occurrence
+// }
 
-export const AboutViolencePageRepository = {
-    AboutViolenceOccurrence,
-}
+// export const AboutViolencePageRepository = {
+//     AboutViolenceOccurrence,
+// }
 
