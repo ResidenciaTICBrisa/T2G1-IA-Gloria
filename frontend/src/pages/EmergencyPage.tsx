4a32c28eb8da79e17941895378c3e13e518d2f69
Izabelle Rivas <izabelle.rivas123@gmail.com>
2024-08-05 10:38:31 -0300

    fix: style correction
    Co-authored-by: izabellerivas <izabelle.rivas123@gmail.com>
    Co-authored-by: barbarabea <barbarabeatrizsilva6@gmail.com>

--- frontend/src/pages/EmergencyPage.tsx
+++ frontend/src/pages/EmergencyPage.tsx
@@ -25,16 +25,19 @@ const EmergencyPage = () => {
     return (
         <section>
             <Header/>
-            <main className="main">
-                <section className="question-emergency"> 
-                    <h3>Você está em risco no momento?</h3>
-                    <p>O número 190 é o telefone da Polícia Militar que deve ser acionado em casos de necessidade imediata ou socorro rápido.</p>
-                </section>
+            <main>
+                <div className="container-emergency">
+                    <section className="question-emergency"> 
+                        <h3>Você está em risco no momento?</h3>
+                        <p>O número 190 é o telefone da Polícia Militar que deve ser acionado em casos de necessidade imediata ou socorro rápido.</p>
+                    </section>
 
-                <section className="buttons-container-emergency">
-                    <button className="green-button" onClick={callPolice}>Sim, ligar 190</button>
-                    <button className="red-button" onClick={callWomanService}>Não, realizar uma denúncia</button>
-                </section>
+                    <section className="buttons-container-emergency">
+                        <button className="green-button" onClick={callPolice}>Sim, ligar 190</button>
+                        <button className="red-button" onClick={callWomanService}>Não, realizar uma denúncia</button>
+                    </section>
+                </div>
+                
             </main>
         </section>
     );
