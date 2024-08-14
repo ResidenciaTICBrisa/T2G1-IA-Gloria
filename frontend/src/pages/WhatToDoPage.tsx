15154f05df91ec6e88507b8bd2c13b86d1ee15e7
Beatriz-ge <bibi.geane2@gmail.com>
2024-08-14 11:55:34 -0300

    feat: what to do page modal

--- frontend/src/pages/WhatToDoPage.tsx
+++ frontend/src/pages/WhatToDoPage.tsx
@@ -1,31 +1,58 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import HeaderPages from "../components/HeaderPages";
 import { FaHand } from "react-icons/fa6";
 import { IoLocationOutline } from "react-icons/io5";
 import { TiPencil } from "react-icons/ti";
 import { MdOutlineSos } from "react-icons/md";
 import { useNavigate } from 'react-router-dom';
+import { MapContainer, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 import '../styles/WhatToDoPage.css';
+import PopUp from '../components/Popup';
 
 const WhatToDoPage = () => {
     const navigate = useNavigate();
+    const [showPopUp, setShowPopUp] = useState(true);
 
     useEffect(() => {
         localStorage.removeItem('selectedState');
         localStorage.removeItem('selectedCity');
-        localStorage.removeItem('date')
-        localStorage.removeItem('time')
-        localStorage.removeItem('ageRange')
-        localStorage.removeItem('checkedItems')
+        localStorage.removeItem('date');
+        localStorage.removeItem('time');
+        localStorage.removeItem('ageRange');
+        localStorage.removeItem('checkedItems');
     }, []);
+
+    const handleAuthorize = () => {
+        setShowPopUp(false);
+    };
+
+    const handleNotAuthorize = () => {
+        setShowPopUp(false);
+    };
 
     return (
         <div className="whattodo-page">
+            <MapContainer
+                center={[-15.794, -47.882]}
+                zoom={12}
+                style={{ width: '100vw', height: '100vh', position: 'absolute', zIndex: '-1' }}
+            >
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+            </MapContainer>
+
             <section>
                 <HeaderPages/>
             </section>
-    
-        <main className="main-whattodo">
+
+            {showPopUp && (
+                <PopUp onAuthorize={handleAuthorize} onNotAuthorize={handleNotAuthorize} />
+            )}
+
+            <main className="main-whattodo">
                 <section className="what-to-do">
                     <section className="titles-whattodo">
                         <h4 className="intro-title">Este é o Mapa da Violência do Instituto Glória</h4>
