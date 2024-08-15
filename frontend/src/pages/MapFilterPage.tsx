c5d0fc9620ed550ed3e11ee4e6f92c4bec1c6905
Beatriz-ge <bibi.geane2@gmail.com>
2024-08-15 11:50:32 -0300

    fix: passing map filter page coordinates

--- frontend/src/pages/MapFilterPage.tsx
+++ frontend/src/pages/MapFilterPage.tsx
@@ -9,16 +9,59 @@ import HeaderMap from '../components/HeaderMap';
 import Pin from '../components/Pin';
 // import { text } from 'stream/consumers';
 import LegendMapFilter from '../components/LegendMapFilter';
-import UserLocation from '../components/UserLocation';
+import UserLocation from '../components/UserLocation'
 import { IoChevronBackCircleSharp } from "react-icons/io5";
+import axios from "axios";
+
+interface Coordinates {
+  lat: number;
+  lon: number;
+}
+
+interface Occurrence {
+  latitude: number;
+  longitude: number;
+  violence_type: string;
+}
+
+const URL = "http://localhost:4000/map-filter";
 
 function MapFilter() {
   const navigate = useNavigate();
   const location = useLocation();
-  const { coordinates, occurrence_data_list } = location.state || {};
+
+  let coordinates: Coordinates | null;
+  let zoom_init: number;
+  if (sessionStorage.getItem('autorizou-localizacao') === "yes"){
+    coordinates = {
+      lat: Number(sessionStorage.getItem('latitude')),
+      lon: Number(sessionStorage.getItem('longitude'))
+    }
+    zoom_init = 14
+  } else {
+    zoom_init = 4
+    coordinates = null;
+  }
+
   const [isFilterVisible, setIsFilterVisible] = useState(false);
   const [selectedFilters, setSelectedFilters] = useState<string[]>([]);
   const [selectedFiltersBackend, setSelectedFiltersBackend] = useState<string[]>([]);
+  const [getOccurrence, setGetOccurrence] = useState(true);
+  const [occurrence_data_list,setOccurrence_data_list] = useState([]);
+  const [mapZoom, setMapZoom] = useState(zoom_init); 
+
+  if (getOccurrence){
+    // Realiza a requisição get para pegar todas as ocorrências
+    axios.get(URL)
+    .then(occurrence_data => {
+      setOccurrence_data_list(occurrence_data.data)
+      setGetOccurrence(false)
+    })
+    .catch(error => {
+      console.log(error);
+      console.log("Serviço indisponível");
+    });
+  }
 
   const violenceMapping: { [key: string]: string } = {
     VT1: 'Física',
@@ -40,17 +83,22 @@ function MapFilter() {
   const formated_occurrence_data = occurrence_data_list.filter((obj: { latitude: number, longitude: number, violence_type: string }) => 
     selectedFiltersBackend.length === 0 || selectedFiltersBackend.some(filter => obj.violence_type.includes(filter)));
 
-  useEffect(() => {
-    if (!coordinates) {
-      // If coordinates are not available, navigate back to the authorization page
-      navigate("/authorize-localization");
-    }
-  }, [coordinates, navigate]);
+  // useEffect(() => {
+  //   if (!coordinates) {
+  //     // If coordinates are not available, navigate back to the authorization page
+  //     navigate("/authorize-localization");
+  //   }
+  // }, [coordinates, navigate]);
+
+  const mapCenter = coordinates || { lat: -15.794, lon: -47.882 };
 
   return (
     <div className="map">
       <div className="overlay-container">
         <HeaderMap/>
+          <button className="button-back-map" onClick={() => navigate(-1)}>
+            <IoChevronBackCircleSharp className="icon-back-map" />
+          </button>
 
         <div className="map-title">
           <p className="map-text" onClick={() => setIsFilterVisible(!isFilterVisible)}>
@@ -77,8 +125,8 @@ function MapFilter() {
       </div>
 
       <MapContainer
-        center={coordinates ? [coordinates.lat, coordinates.lon] : [-15.794, -47.882]}
-        zoom={14}
+        center={mapCenter ? [mapCenter.lat, mapCenter.lon] : [-15.794, -47.882]}
+        zoom={zoom_init}
         style={{ width: '100vw', height: '100vh' }}
         zoomControl={false}
       >
@@ -99,7 +147,7 @@ function MapFilter() {
           </Marker>
         ))}
 
-        <LegendMapFilter />
+      
       </MapContainer>
 
       <LegendMapFilter />
